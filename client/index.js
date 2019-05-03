9c7385cc4cf0708efb7bb3171ce4d9cd0e5a26de
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-05-03 14:42:12 -0700

    Set up package.json, webpack, and html file

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'open table reviews';
+
+ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public'
+  }
+};
