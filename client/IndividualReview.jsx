d12e56247a5cc158a106c1a3a8ec7388f68d953b
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-05-19 00:30:57 -0700

    Create star component for individual reviews and dynamically render date

--- client/IndividualReview.jsx
+++ client/IndividualReview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-// import ReviewList from './ReviewList';
+
+import IndividualStars from './IndividualStars.jsx';
 
 const IndividualReview = (props) => {
 
@@ -11,6 +12,9 @@ const IndividualReview = (props) => {
   const privacy = props.review.privacy
   const name = props.review.first_name + props.review.last_name.charAt(0);
   const username = props.review.username
+  const city = props.review.location.split(',')[0];
+  const reviewsWritten = props.review.reviews_written;
+  const date = props.review.date.split('').slice(4, 15).join('');
 
   const iconDisplay = () => {
     if (privacy === true) {
@@ -45,7 +49,20 @@ const IndividualReview = (props) => {
               <Name>{displayName()}</Name>
             </NameSpan>
           </UserInfo>
+          <City>{city}</City>
+          <UserReviewsWrittenDiv>
+            <ReviewIcon><i className="far fa-comment-alt"></i></ReviewIcon>
+            <ReviewNumber>{reviewsWritten} reviews</ReviewNumber>
+          </UserReviewsWrittenDiv>
         </UserInfoContainer>
+        <ReviewContextContainer>
+          <IndividualRatingDiv>
+            <StarsDateContainer>
+              <IndividualStars review={props.review}/>
+              <Date>Dined on {date}</Date>
+            </StarsDateContainer>
+          </IndividualRatingDiv>
+        </ReviewContextContainer>
       </ReviewContainer>
 
 
@@ -57,75 +74,137 @@ const IndividualReview = (props) => {
 export default IndividualReview;
 
 const Review = styled.div`
-        display: flex;
-        font-family: BrandonText,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
-        -webkit-font-smoothing: antialiased;
-      `;
+  display: flex;
+  font-family: BrandonText,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+  -webkit-font-smoothing: antialiased;
+`;
 
 const ReviewContainer = styled.div`
-        display: flex;
-        flex-direction: row;
-        width: 100%;
-        padding-top: 1rem;
-        padding-bottom: 1rem;
-        border-bottom: solid 1px #d8d9db;
-        color: #2d333f;
-      `;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: solid 1px #d8d9db;
+  color: #2d333f;
+`;
 
 const UserInfoContainer = styled.div`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-right: 1rem;
-        margin-left: 1rem;
-        width: 6rem;
-        position: relative;
-        flex: none;
-      `;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 1rem;
+  margin-left: 1rem;
+  width: 6rem;
+  position: relative;
+  flex: none;
+`;
 
 const UserIconDiv = styled.div`
-        display: flex;
-        justify-content: center;
-        margin-bottom: 0.5rem;
-      
-      `;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 0.5rem;
+
+`;
 
 const UserIcon = styled.div`
-        background: #d86441;
-        width: 3rem;
-        height: 3rem;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      `;
+  background: #d86441;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const UserInitial = styled.div`
-        color: #ffffff;
-        font-weight: 500;
-        text-transform: capitalize;
-      `;
+  color: #ffffff;
+  font-weight: 500;
+  text-transform: capitalize;
+`;
 
 const UserInfo = styled.div`
-      font-weight: 500;
-      font-size: 0.875rem;
-      text-align: center;
-      color: #2d333f;
-      margin-bottom: 0.25rem;
-      
-      `;
+  font-weight: 500;
+  font-size: 0.875rem;
+  text-align: center;
+  color: #2d333f;
+  margin-bottom: 0.25rem;
+  
+  `;
 
 const NameSpan = styled.span`
-        font-weight: 500;
-        font-size: 0.875rem;
-        text-align: center;
-      `;
+  font-weight: 500;
+  font-size: 0.875rem;
+  text-align: center;
+`;
 
 const Name = styled.span`
-        font-weight: 500;
-        font-size: 0.875rem;
-        text-align: center;
-        color: #2d333f;
-        display: block;
-        word-break: break-word;
+  font-weight: 500;
+  font-size: 0.875rem;
+  text-align: center;
+  color: #2d333f;
+  display: block;
+  word-break: break-word;
+`;
+
+const City = styled(NameSpan)`
+  line-height: 1.43;
+  color: #6f737b;
+  margin-bottom: 0.5rem;
+
+`;
+
+const UserReviewsWrittenDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 1.43;
+  flex: none;
+  white-space: nowrap;
+  color: #6f737b;
+`;
+
+const ReviewIcon = styled.span`
+  display: inline-block;
+  position: relative;
+  height: 16px;
+  width: 16px;
+  flex: none;
+  margin-right: 0.25rem;
+`;
+
+const ReviewNumber = styled.span`
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 1.43;
+  white-space: nowrap;
+  color: #6f737b;
+`;
+
+const ReviewContextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const IndividualRatingDiv = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+`;
+
+const StarsDateContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 0.875rem;
+  font-weight: 500;
+`;
+
+const Date = styled(StarsDateContainer)`
+  margin: 0 0.25rem;
+  font-size: 0.875rem;
+  line-height: 1.43;
+  font-weight: 500;
 `;
---
+++ client/IndividualStars.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const IndividualStars = (props) => {
+
+  const userStarsArray = [];
+  const userRating = props.review.individual_rating
+  // console.log('individual', userRating)
+  const userOverall = props.review.individual_rating.Overall;
+
+  const renderUserStars = (number) => {
+    for (let i = 0; i < 5; i++) {
+      if (number >= 1) {
+        userStarsArray.push('whole');
+      } else {
+        userStarsArray.push('empty');
+      }
+      number--
+    }
+    return userStarsArray;
+  }
+
+  renderUserStars(userOverall)
+
+  const colorStars = userStarsArray.map((star) => {
+    if (star === 'whole') {
+      return (<Fill><i className="fas fa-star"></i></Fill>)
+    } else {
+      return (<Empty><i className="fas fa-star"></i></Empty>)
+    }
+  })
+
+  return (
+    <StarsDiv>
+      <Stars>{colorStars}</Stars>
+    </StarsDiv>
+  )
+};
+
+export default IndividualStars;
+
+const StarsDiv = styled.div`
+  width: calc(16px * 5 + 0.25rem * 5);
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Fill = styled.div`
+width: 16px;
+height: 16px;
+margin-right: 0.25rem;
+color: #da3743;
+
+`;
+
+const Empty = styled.div`
+width: 16px;
+height: 16px;
+margin-right: 0.25rem;
+color: #d8d9db
+`;
+
+const Stars = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 16px;
+  height: 16px;
+  margin-right: 0.25rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+`;
