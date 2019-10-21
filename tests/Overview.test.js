815a39e26fbac033f0c07b7a16caad3b7a38a5d3
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-10-21 01:09:16 -0700

    Add additional tests

--- tests/Overview.test.js
+++ tests/Overview.test.js
@@ -1,18 +1,33 @@
-import React from 'react';
 import Overview from '../client/Overview';
-import { shallow, render, mount } from 'enzyme';
-
 
 describe('Overview', () => {
   it("should render Overview component", () => {
-    const wrapper = shallow(<Overview />)
+    const wrapper = shallow(<Overview />);
     expect(wrapper.exists()).toBe(true)
   });
-})
 
-// describe('Overview', () => {
-//   it("should render Overview component", () => {
-//     const wrapper = shallow(<Overview />)
-//     expect(wrapper.find(".overview").text()).toEqual('Overall ratings and reviews')
-//   });
-// })
+  it("should detect child component", () => {
+    const wrapper = shallow(<Overview />);
+    //console.log('wrapper', wrapper.debug())
+    expect(wrapper.find('BarGraph').exists()).toBe(true)
+  });
+
+  it("should check if reviews exist in state", () => {
+    const wrapper = shallow(<Overview />);
+    expect(wrapper.state()).toHaveProperty('reviews')
+  });
+
+  it("should pass correct rating value to state after handleFilterClick method", () => {
+    let id = 5
+    const component = mount(<Overview />);
+    component.instance().handleFilterClick(id)
+    expect(component.state().idForBar).toBe(5)
+  });
+
+  it("should call componentDidMount once", () => {
+    const componentDidMountSpy = jest.spyOn(Overview.prototype, 'componentDidMount');
+    const wrapper = mount(<Overview />);
+    expect(componentDidMountSpy).toHaveBeenCalledTimes(1);
+  });
+
+});
