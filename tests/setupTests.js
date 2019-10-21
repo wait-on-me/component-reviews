1e8d5f638dc08cdc6c82fe2d25debbdee06c87ad
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-10-21 01:08:49 -0700

    Add global variables in config file

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,14 +1,10 @@
-import { configure } from 'enzyme';
+import React from 'react';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-// import React from 'react';
-// import Enzyme, { shallow, render, mount } from 'enzyme';
-// // import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-// global.React = React;
-// global.shallow = shallow;
-// global.render = render;
-// global.mount = mount;
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
